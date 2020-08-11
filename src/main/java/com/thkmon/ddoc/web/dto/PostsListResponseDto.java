cdced5f95d9acaf26fb2e6588b1ac9a9aca2d0a5
thkmon <thkmon@gmail.com>
2020-08-11 23:50:06 +0900

    게시글 등록 기능까지 완료

--- src/main/java/com/thkmon/ddoc/web/dto/PostsListResponseDto.java
+++ src/main/java/com/thkmon/ddoc/web/dto/PostsListResponseDto.java
@@ -2,19 +2,18 @@ package com.thkmon.ddoc.web.dto;
 
 import com.thkmon.ddoc.domain.posts.Posts;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class PostsListResponseDto {
     private Long id;
     private String title;
     private String author;
-    private LocalDate modifiedDate;
+    private LocalDateTime modifiedDate;
 
     public PostsListResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
-        // this.modifiedDate = entity.getModifiedDate();
+        this.modifiedDate = entity.getModifiedDate();
     }
 }
