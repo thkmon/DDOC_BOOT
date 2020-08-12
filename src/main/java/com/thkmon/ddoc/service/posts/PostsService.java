c19c9829978306a5008fd1e145d4687686efdd92
thkmon <thkmon@gmail.com>
2020-08-12 23:49:31 +0900

    게시글 조회 기능 작업

--- src/main/java/com/thkmon/ddoc/service/posts/PostsService.java
+++ src/main/java/com/thkmon/ddoc/service/posts/PostsService.java
@@ -2,11 +2,16 @@ package com.thkmon.ddoc.service.posts;
 
 import com.thkmon.ddoc.domain.posts.Posts;
 import com.thkmon.ddoc.domain.posts.PostsRepository;
+import com.thkmon.ddoc.web.dto.PostsListResponseDto;
 import com.thkmon.ddoc.web.dto.PostsResponseDto;
 import com.thkmon.ddoc.web.dto.PostsSaveRequestDto;
 import com.thkmon.ddoc.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -20,17 +25,16 @@ public class PostsService {
 
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다.  id=" + id));
-
-        //
-
         return id;
     }
 
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
-
         return new PostsResponseDto(entity);
     }
 
-    //public List<PostsLi>
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAll().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
 }
